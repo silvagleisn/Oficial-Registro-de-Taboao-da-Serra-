4373a3b90bcedaab097d1763e7130e9c739a393b
cartorio <silvagleisn>
2020-01-27 16:46:24 -0300

    Projeto Registro Taboao

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+sql_autoload_register(function($class_name){
+
+    $filename = "class".DIRECTORY_SEPARETOR. $class_name.".php";
+
+    if(file_exists(($filename))) {
+        require_one($filename);
+
+    }
+
+
+});
+
+
+?>
